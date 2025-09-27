2fd78004c1948fd0c364da90fdc7f469f6d211d3
Andris <andrewabrahamproduction@gmail.com>
2025-09-27 12:54:21 +0200

    Elso

---
+++ triangle.py
@@ -0,0 +1,24 @@
+import turtle
+
+def rajzol() :
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.goto(-75, 0)
+    turtle.pendown()
+    turtle.pendown()
+    #turtle.right(30)
+    for _ in range(3):
+        turtle.forward(150)
+        turtle.left(120)
+
+ablak = turtle.Screen()
+turtle.hideturtle()
+turtle.bgcolor("green")
+turtle.pensize(10)
+turtle.pencolor("red")
+turtle.listen()
+turtle.onkey(rajzol, "h")
+turtle.onkey(turtle.bye,"q")
+
+turtle.mainloop()
+
