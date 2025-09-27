090639d0bfe73b76e016be939d42521f7c64cacb
Andris <andrewabrahamproduction@gmail.com>
2025-09-27 13:20:38 +0200

    Elso

--- penta.py
+++ penta.py
@@ -5,16 +5,16 @@ def rajzol():
     turtle.penup()
     turtle.goto(-75, 0)
     turtle.pendown()
-    
+
     for _ in range(5):
         turtle.forward(100)
         turtle.left(72)
 
 ablak = turtle.Screen()
 turtle.hideturtle()
-turtle.bgcolor("green")
+turtle.bgcolor("black")
 turtle.pensize(10)
-turtle.pencolor("red")
+turtle.pencolor("green")
 turtle.listen()
 turtle.onkey(rajzol, "h")
 turtle.onkey(turtle.bye, "q")  #
